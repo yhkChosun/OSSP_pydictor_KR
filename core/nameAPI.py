3bb99694509986ed285a2412cfe2d3a5b8a06f4c
yhkChosun <105004850+yhkChosun@users.noreply.github.com>
2023-01-16 19:44:29 +0900

    Modified nameAPI.py

--- core/nameAPI.py
+++ core/nameAPI.py
@@ -1,24 +1,26 @@
 import json
 import urllib.request
 
-client_id = "acENPxjnFVHGMHuoxfSH"
-client_secret = "t7gvv26VdB"
+def translateName(name):
+    client_id = "acENPxjnFVHGMHuoxfSH"
+    client_secret = "t7gvv26VdB"
 
-encText = urllib.parse.quote("배수지")
-url = "https://openapi.naver.com/v1/krdict/romanization?query=" + encText
+    encText = urllib.parse.quote(name)
+    url = "https://openapi.naver.com/v1/krdict/romanization?query=" + encText
 
-request = urllib.request.Request(url)
-request.add_header("X-Naver-Client-Id",client_id)
-request.add_header("X-Naver-Client-Secret",client_secret)
-response = urllib.request.urlopen(request)
-rescode = response.getcode()
+    request = urllib.request.Request(url)
+    request.add_header("X-Naver-Client-Id",client_id)
+    request.add_header("X-Naver-Client-Secret",client_secret)
+    response = urllib.request.urlopen(request)
+    rescode = response.getcode()
 
-if(rescode==200):
-    response_body = response.read()
-    json_dict = json.loads(response_body.decode('utf-8'))
-    result = json_dict['aResult'][0]
-    name_items = result['aItems']
-    names = [name_item['name'] for name_item in name_items]
-    print(names)
-else:
-    print("Error Code:" + rescode)
+    if(rescode==200):
+        response_body = response.read()
+        json_dict = json.loads(response_body.decode('utf-8'))
+        result = json_dict['aResult'][0]
+        name_items = result['aItems']
+        names = [name_item['name'] for name_item in name_items]
+        print(names)
+    else:
+        print("Error Code:" + rescode)
+    return names
