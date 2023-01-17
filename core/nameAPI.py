c2e22d312955e0a1f490d112c6141f1d0b1baf75
yhkChosun <105004850+yhkChosun@users.noreply.github.com>
2023-01-17 12:17:40 +0900

    Update nameAPI.py

--- core/nameAPI.py
+++ core/nameAPI.py
@@ -1,6 +1,7 @@
 import json
 import urllib.request
 
+# translateName 메소드는 한글인명로마자 변환 API를 이용하여 한글 이름을 영어 이름으로 변환시키는 메소드이다.
 def translateName(name):
     client_id = "acENPxjnFVHGMHuoxfSH"
     client_secret = "t7gvv26VdB"
@@ -9,12 +10,12 @@ def translateName(name):
     url = "https://openapi.naver.com/v1/krdict/romanization?query=" + encText
 
     request = urllib.request.Request(url)
-    request.add_header("X-Naver-Client-Id",client_id)
-    request.add_header("X-Naver-Client-Secret",client_secret)
+    request.add_header("X-Naver-Client-Id", client_id)
+    request.add_header("X-Naver-Client-Secret", client_secret)
     response = urllib.request.urlopen(request)
     rescode = response.getcode()
 
-    if(rescode==200):
+    if rescode == 200:
         response_body = response.read()
         json_dict = json.loads(response_body.decode('utf-8'))
         result = json_dict['aResult'][0]
